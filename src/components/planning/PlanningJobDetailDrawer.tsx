2a111f79a3c81e504955408ba7543cce4d7fde33
Anik Dua <dua.anik@gmail.com>
2026-05-26 10:43:28 +0530

    fix(planning): reset warehouse reserved-by-material map on line change

--- src/components/planning/PlanningJobDetailDrawer.tsx
+++ src/components/planning/PlanningJobDetailDrawer.tsx
@@ -472,6 +472,7 @@ export function PlanningJobDetailDrawer({
     setOptionDetailsByMaterial({})
     setOptionDetailsLoading({})
     setSuggestionsWorkspaceOpen(false)
+    setLineReservedByMaterial({})
   }, [line?.id])
 
   useEffect(() => {
